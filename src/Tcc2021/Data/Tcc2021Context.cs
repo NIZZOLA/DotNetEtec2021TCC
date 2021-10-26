265f0a0fa7154016074d484ac7a1596bd048862b
nizzola <marcio@nizzola.com.br>
2021-10-26 20:32:01 -0300

    Criação do projeto

---
+++ src/Tcc2021/Data/Tcc2021Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tcc2021.Models
+{
+    public class Tcc2021Context : DbContext
+    {
+        public Tcc2021Context (DbContextOptions<Tcc2021Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tcc2021.Models.UsuarioModel> UsuarioModel { get; set; }
+    }
+}
--- src/Tcc2021/Startup.cs
+++ src/Tcc2021/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Tcc2021.Models;
 
 namespace Tcc2021
 {
@@ -33,6 +35,9 @@ namespace Tcc2021
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Tcc2021Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Tcc2021Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
